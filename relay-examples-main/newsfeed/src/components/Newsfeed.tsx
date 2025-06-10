e50422f10eb8b1d88228e04a1a931676527c2a0a
hohodsj <hohodsj@gmail.com>
2025-06-09 23:35:20 -0400

    To query with relay

--- relay-examples-main/newsfeed/src/components/Newsfeed.tsx
+++ relay-examples-main/newsfeed/src/components/Newsfeed.tsx
@@ -2,6 +2,8 @@ import * as React from "react";
 import Story from "./Story";
 
 import { graphql } from "relay-runtime";
+import { useLazyLoadQuery } from "react-relay";
+
 
 const NewsfeedQuery = graphql`
   query NewsfeedQuery {
@@ -22,19 +24,11 @@ const NewsfeedQuery = graphql`
 `;
 
 export default function Newsfeed() {
-  const story = {
-    title: "Placeholder Story",
-    summary: "Placeholder data, to be replaced with data fetched via GraphQL",
-    poster: {
-      name: "Placeholder Person",
-      profilePicture: {
-        url: "/assets/cat_avatar.png",
-      },
-    },
-    thumbnail: {
-      url: "/assets/placeholder.jpeg",
-    },
-  };
+  const data = useLazyLoadQuery(
+    NewsfeedQuery,
+    {},
+  );
+  const story = data.topStory;
 
   return (
     <div className="newsfeed">
